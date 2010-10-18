f9af13a7f17ffabf3acafa4ccbd772a7a3040385
frankpw <frankpw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-18 19:52:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fpw-category-thumbnails/trunk@301333 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,9 @@
 <?php
-if ( defined( 'WP_UNINSTALL_PLUGIN' ) && get_option( 'fpw_category_thumb_del' ) )  {
-	delete_option( 'fpw_category_thumb_del' );
-	delete_option( 'fpw_category_thumb_ids' );
+$opt = get_option( 'fpw_category_thumb_opt' );
+if ( is_array( $opt ) )
+	$opt = $opt[ 'clean' ];
+if ( defined( 'WP_UNINSTALL_PLUGIN' ) && $opt ) {
+	delete_option( 'fpw_category_thumb_opt' );
+	delete_option( 'fpw_category_thumb_map' );
 }
 ?>
